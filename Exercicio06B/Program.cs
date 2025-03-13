9fb29cd38ffc642d60c4fd6903975f8a76b23068
LINCOLN GABRIEL SOUSA MATOS <lincoln.matos@cs.up.edu.br>
2025-03-13 10:38:28 -0300

    Exercicio 06b

--- Exercicio06B/Program.cs
+++ Exercicio06B/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int MAX_VAL = 10;
+int[] array = new int[10];
+
+PreencherArray(array);
+Console.WriteLine("UNSORTED:");
+PrintarArray(array);
+
+Array.Sort(array);
+Console.WriteLine("SORTED:");
+PrintarArray(array);
+
+static void PreencherArray(int[] array)
+{
+    Random random = new();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        int r = random.Next(MAX_VAL);
+        array[i] = r;
+    }
+}
+
+static void PrintarArray(int[] array) {
+    Console.Write("{");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + (i < array.Length - 1 ? ", " : ""));
+    }
+
+    Console.WriteLine("}");
+}
