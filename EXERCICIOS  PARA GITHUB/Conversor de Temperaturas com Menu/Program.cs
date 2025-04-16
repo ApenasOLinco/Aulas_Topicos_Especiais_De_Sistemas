847a2bb483e4c7a21ead74fc8bcb3e327d7baec2
ApenasOLinco <lincoln.matos1003@gmail.com>
2025-04-16 19:23:21 -0300

    Verificadordor de palíndromos pronto

--- EXERCICIOS  PARA GITHUB/Conversor de Temperaturas com Menu/Program.cs
+++ EXERCICIOS  PARA GITHUB/Conversor de Temperaturas com Menu/Program.cs
@@ -1,4 +1,49 @@
 ﻿/*
-    
+    Crie um menu com as opções:
+        1 - Celsius para Fahrenheit
+        2 - Fahrenheit para Celsius
+        3 - Sair
+    Use switch e while para manter o menu ativo até o usuário sair
 */
 
+while (true)
+{
+    float unconverted = 0;
+
+    Console.WriteLine(
+    """
+    1 - Celsius para Fahrenheit
+    2 - Fahrenheit para Celsius
+    3 - Sair
+    """
+    );
+
+    int opcao = int.Parse(Console.ReadLine());
+
+    if(opcao != 3) {
+        Console.WriteLine($"Insira o valor a ser convertido: ");
+        unconverted = float.Parse(Console.ReadLine());
+    }
+
+    float result = 0;
+    switch (opcao)
+    {
+        case 1:
+            result = (unconverted * 1.8F) + 32F;
+            break;
+
+        case 2:
+            result = (unconverted - 32F) / 1.8F;
+            break;
+
+        case 3:
+            Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine($"Entrada inválida! Escolha uma das opções disponíveis.");
+            continue;
+    }
+
+    Console.WriteLine($"{unconverted}°{(opcao == 1 ? "C" : "F")} = {result}°{(opcao == 1 ? "F" : "C")}");
+}
---
+++ EXERCICIOS  PARA GITHUB/Verificador de Palíndromos/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Peça ao usuário para digitar uma palavra ou frase e verifique se ela é um palíndromo
+(lê-se igual de frente para trás e de trás para frente, desconsiderando espaços e acentos)
+*/
+
+using System.Globalization;
+using System.Text;
+
+string? expressao, reversa;
+
+Console.WriteLine("Insira uma palavra ou frase: ");
+expressao = Console.ReadLine() ?? "";
+
+expressao = RemoverAcentos(expressao)
+    .ToLower()
+    .Replace(" ", "");
+
+
+reversa = new([..
+    expressao
+        .Normalize()
+        .Reverse()
+]);
+
+
+if (reversa.Equals(expressao))
+{
+    Console.WriteLine("É palíndromo!");
+}
+else
+{
+    Console.WriteLine("Não é palíndromo.");
+}
+
+
+static string? RemoverAcentos(string? expressao)
+{
+    if (string.IsNullOrWhiteSpace(expressao)) throw new Exception("A expressão digitada é vazia!");
+
+    expressao = expressao.Normalize(NormalizationForm.FormD);
+    StringBuilder resultado = new();
+
+    foreach (char c in expressao)
+    {
+        var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+
+        if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+            resultado.Append(c);
+    }
+
+    return resultado.ToString();
+}
